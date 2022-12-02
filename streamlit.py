d8c5ce202cbbc6038b9d7f002051d79627e6b3bd
TrindadeD <david.trindade2000@gmail.com>
2022-12-02 18:37:08 +0000

    k

--- streamlit.py
+++ streamlit.py
@@ -1,24 +1,47 @@
-from paho.mqtt import client as mqtt_client
 import streamlit as st
+#import pandas as pd
 import numpy as np
+from paho.mqtt import client as mqtt_client
 
+st.title('Extração de Features de um ficheiro áudio')
 
-#st.title('Extração de Features de um ficheiro áudio')
+#################### GRAVAR E GUARDAR FICHEIRO DE SOM #########################
 
-broker = 'mqtt.eclipseprojects.io'
-port = 1883
-topic = "projeto_aaib" 
+import sounddevice as sd
+from scipy.io.wavfile import write
 
-client = mqtt_client.Client()
-client.connect(broker, port)
+########################### GRAVAR AUDIO ######################################
 
-def publish(client):
-    inicio = 'sad'
-    result = client.publish(topic, inicio)
-    status = result[0]
-    if status == 0:
-        print(f"Message sent to topic `{topic}`")
-    else:
-        print(f"Failed to send message to topic {topic}")
-        
-publish(client)
+if st.button('Gravar Áudio'):
+    
+    broker = 'mqtt.eclipseprojects.io'
+    port = 1883
+    topic = "projeto_aaib"
+    
+    def connect_mqtt():
+        def on_connect (client, userdata, flags, rc):
+            if rc==0:
+                st.print('connected')
+            else:
+                st.print("Failed to connect, return code %d\n", rc)
+        client=mqtt_client.Client()
+        client.on_connect = on_connect
+        client.connect(broker, port)
+        return client
+    
+    def publish(client):
+        som=('a')
+        result = client.publish(topic, som)
+        status = result[0]
+        if status == 0:
+            print(f"som")
+        else:
+            print(f"Failed to send message to topic {topic}")
+    def run():
+        client = connect_mqtt()
+        client.loop_start()
+        publish(client)
+        client.loop_stop()
+
+    if __name__ == '__main__':
+        run()
